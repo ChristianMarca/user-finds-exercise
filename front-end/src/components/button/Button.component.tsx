877cc0bc98e53b963a7131e9379208fc4f1051ac
Abdullah Tayel <abdullah@tayel.com>
2022-12-14 16:24:55 +0200

    feat: add simple button component

---
+++ front-end/src/components/button/Button.component.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: rgb(212 212 216);
+  text-transform: uppercase;
+  padding: 0.5rem 1rem;
+  &:hover {
+    background-color: rgb(228 228 231);
+  }
+`;
