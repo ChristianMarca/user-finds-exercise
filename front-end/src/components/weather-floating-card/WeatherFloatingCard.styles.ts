9cad06c4db9aafddf5fe266ece520cb66ce147a8
Abdullah Tayel <abdullah@tayel.com>
2022-12-15 12:38:58 +0200

    fix: remove unused components

--- front-end/src/components/weather-floating-card/WeatherFloatingCard.styles.ts
+++ front-end/src/components/weather-floating-card/WeatherFloatingCard.styles.ts
@@ -26,21 +26,6 @@ export const Card = styled.div`
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
 `;
 
-export const CardTitle = styled.h5`
-  margin-bottom: 0.5rem;
-  color: #111827;
-  font-size: 1.5rem;
-  line-height: 2rem;
-  font-weight: 700;
-  letter-spacing: -0.025em;
-`;
-
-export const CardParagraph = styled.p`
-  margin-bottom: 0.75rem;
-  color: #374151;
-  font-weight: 400;
-`;
-
 export const DegreeSection = styled(FlexContainer)`
   margin-top: 2.5rem;
   margin-bottom: 1.25rem;
